be15c9f440515795a0f7ccc24cd2192ccf4281fa
WasiaL007 <wasialaiub21@gmail.com>
2026-07-12 03:56:54 +0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,15 @@ echo "Installing WASI AL OS..."
 pkg update -y
 pkg install wget -y
 
-wget https://github.com/WasiaL007/WasiaL007/releases/download/v1.5/WASI_AL_OS_Termux_v1.5.apk
+APK="WASI_AL_OS_Termux_v1.5.apk"
+
+if [ -f "$APK" ]; then
+    echo "APK already exists."
+else
+    wget -O "$APK" https://github.com/WasiaL007/WasiaL007/releases/download/v1.5/WASI_AL_OS_Termux_v1.5.apk
+fi
 
 echo "Download Complete!"
-echo "Open the APK file and install WASI AL OS."
+echo "Opening WASI AL OS installer..."
+
+am start -a android.intent.action.VIEW -d file://$PWD/$APK -t application/vnd.android.package-archive
